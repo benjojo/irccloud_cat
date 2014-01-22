e362ec07f1c6b0885f84b811ad0d885ae5a14c31
Ben Cox <ben@benjojo.co.uk>
2014-01-22 23:15:10 +0000

    gah, Futile First commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	fmt.Fprintf(os.Stderr, "IRCCloud Streamer")
+	h, e := http.NewRequest("GET", "https://www.irccloud.com/chat/stream", nil)
+	if e != nil {
+		panic(e)
+	}
+
+	h.Header.Add("Cookie", "session=asdfasdfasd")
+
+	reader := bufio.NewReader(resp.Body)
+	for {
+		s, err := reader.ReadString("\n")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s", s)
+	}
+}
