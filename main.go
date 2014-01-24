92fd76eabbb44dfbb87195a6b2e843e30ef79fbd
Ben Cox <ben@benjojo.co.uk>
2014-01-24 18:13:22 +0000

    You can now give the session as a cmd arg as well
    Fixes #1

--- main.go
+++ main.go
@@ -19,21 +19,26 @@ type MessageDump struct {
 
 func main() {
 	fmt.Fprintf(os.Stderr, "IRCCloud Streamer")
-
-	sb, e := ioutil.ReadFile("./.session")
-	if e != nil {
-		log.Fatal("cannot read session cookie. Please make a .session file and put your cookie in there.")
-	}
-	lines := strings.Split(string(sb), "\n")
-	if len(lines) != 1 {
-		log.Fatal("There is more than one line in the session file. wat.")
+	streamtoken := ""
+	if len(os.Args) == 2 {
+		streamtoken = os.Args[1]
+	} else {
+		sb, e := ioutil.ReadFile("./.session")
+		if e != nil {
+			log.Fatal("cannot read session cookie. Please make a .session file and put your cookie in there.")
+		}
+		lines := strings.Split(string(sb), "\n")
+		if len(lines) != 1 {
+			log.Fatal("There is more than one line in the session file. wat.")
+		}
+		streamtoken = lines[0]
 	}
 	h, e := http.NewRequest("GET", "https://www.irccloud.com/chat/stream", nil)
 	if e != nil {
 		panic(e)
 	}
 	client := &http.Client{}
-	h.Header.Add("Cookie", fmt.Sprintf("session=%s", lines[0]))
+	h.Header.Add("Cookie", fmt.Sprintf("session=%s", streamtoken))
 
 	resp, e := client.Do(h)
 	if e != nil {
@@ -47,7 +52,7 @@ func main() {
 		json.Unmarshal([]byte(s), &wat)
 		if wat.URL != "" && wat.Type == "oob_include" {
 			h, e = http.NewRequest("GET", "https://www.irccloud.com"+wat.URL, nil)
-			h.Header.Add("Cookie", fmt.Sprintf("session=%s", lines[0]))
+			h.Header.Add("Cookie", fmt.Sprintf("session=%s", streamtoken))
 			tresp, _ := client.Do(h)
 			ioutil.ReadAll(tresp.Body)
 			fmt.Fprintf(os.Stderr, "Did the OOB include, the stream should work ~forever~ now.")
